902d05f7f0c59f567e680ac9848cb02c0d301004
Ana Montero <anamongar@gmail.com>
2022-09-03 17:59:48 +0200

    Set up project: include gitignore, and application properties

---
+++ src/main/java/com/example/thestonebank/TheStoneBankFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.thestonebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheStoneBankFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheStoneBankFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thestonebank/TheStoneBankFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thestonebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheStoneBankFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
